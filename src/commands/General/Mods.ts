065af6d320a06aa23b0e90c97b1487549dd143e5
00300 <90952986+well300@users.noreply.github.com>
2021-12-04 17:55:23 +0530

    🎀

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -1,30 +1,51 @@
-import MessageHandler from '../../Handlers/MessageHandler'
-import BaseCommand from '../../lib/BaseCommand'
-import WAClient from '../../lib/WAClient'
-import { ISimplifiedMessage } from '../../typings'
+/** @format */
+
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
 
 export default class Command extends BaseCommand {
-    constructor(client: WAClient, handler: MessageHandler) {
-        super(client, handler, {
-            command: 'mods',
-            description: "Displays the Moderators' contact info",
-            category: 'general',
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "mods",
+			description: "Generally used to check if bot is Up",
+			category: "general",
             usage: `${client.config.prefix}mods`,
-            aliases: ['moderators', 'mod', 'owner']
-        })
-    }
+            dm: true,
+            aliases: ['moderators', 'mod', 'owner'],
+		});
+	}
 
-    run = async (M: ISimplifiedMessage): Promise<void> => {
-        if (!this.client.config.mods || !this.client.config.mods[0]) return void M.reply('*[UNMODERATED]*')
-        const filteredMap = this.client.config.mods.map((mod) => this.client.getContact(mod)).filter((user) => user)
-        let text = '🍥 *Moderators* 🍥\n\n'
-        filteredMap.forEach(
-            (user, index) =>
-                (text += `#${index + 1}\n🌟 *Username: ${
-                    user.notify || user.vname || user.name || 'null'
-                }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
-        )
-        text += `\n🌟 Chitoge 🌟 `
-        return void M.reply(text)
-    }
+	run = async (M: ISimplifiedMessage): Promise<void> => {
+		const kacchan =
+			"https://c.tenor.com/KvfvoRnys6UAAAPo/anime-zero-two.mp4";
+		return void this.client.sendMessage(
+			M.from,
+			{ url: kacchan },
+			MessageType.video,
+			{
+				quoted: M.WAMessage,
+				mimetype: Mimetype.gif,
+				caption: `🎗 ᴍᴏᴅᴇʀᴀᴛᴏʀꜱ \n╰• ᴡᴇʟʟ300/>
+                                          🎀 ᴄᴏɴᴛᴀᴄᴛ🎗: wa.me/917842346461?text=𝙝𝙚𝙡𝙡𝙤
+                                          ──────────────
+                                          ╰• ᴘᴀɪɴ
+                                          🎀 ᴄᴏɴᴛᴀᴄᴛ🎗: wa.me/919662713165?text=𝙝𝙚𝙡𝙡𝙤
+                                          ──────────────
+                                          ╰• ʟᴏʀᴅ ʀᴀᴋᴇꜱʜ
+                                          🎀 ᴄᴏɴᴛᴀᴄᴛ🎗: wa.me/918473956301?text=𝙝𝙚𝙡𝙡𝙤
+                                          ──────────────
+                                          ╰• ʙʟᴀᴢᴇ
+                                          🎀 ᴄᴏɴᴛᴀᴄᴛ🎗: wa.me/917721059975?text=𝙝𝙚𝙡𝙡𝙤
+                                          ──────────────────
+                                          🅂🅄🄿🄿🄾🅁🅃 🄶🅁🄾🅄🄿
+                                        
+                                          🎗 https://chat.whatsapp.com/JboXSiHnBLBLE9cJ5Ai8ur
+                                          ──────────────────
+                                          ──────────────────`,
+			}
+		);
+	};
 }
