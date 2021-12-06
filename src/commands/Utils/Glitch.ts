3d2ff61e38f89bf1baa8aefecfe17fb8b6ee56ec
SᴀʀᴜᴍᴀɴSᴀᴍᴀ <94067379+SarumanSama@users.noreply.github.com>
2021-12-06 15:11:35 +0530

    Update Glitch.ts

--- src/commands/Utils/Glitch.ts
+++ src/commands/Utils/Glitch.ts
@@ -21,7 +21,7 @@ export default class Command extends BaseCommand {
 	}
 
 	run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
-		 if (!joined) return void (await M.reply(`Provide the text, Baka!`))
+		 if (!joined) return void (await M.reply(`Give me the text 🐱`))
         const cara = joined.trim()
 		const wall = await w5botapi.textpro("https://textpro.me/create-impressive-glitch-text-effects-online-1027.html",
     cara
