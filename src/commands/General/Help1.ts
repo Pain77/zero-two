fed400b203750889d11867c564ee5ecd4a9014fb
00300 <90952986+well300@users.noreply.github.com>
2021-12-08 20:20:26 +0530

    GIF UPDATE

--- src/commands/General/Help1.ts
+++ src/commands/General/Help1.ts
@@ -20,7 +20,7 @@ export default class Command extends BaseCommand {
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const zerotwo = 
-			"https://c.tenor.com/KslZTP0KezsAAAPo/zero-two-beach.mp4";
+			"https://c.tenor.com/yvGNPPoeZckAAAPo/darling-in.mp4";
 		return void this.client.sendMessage(
 			M.from,
 			{ url: zerotwo },
