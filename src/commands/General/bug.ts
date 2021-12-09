d1edfd4379b250e39284f74cced267ed6bc027a9
Pratyush <86536877+pratyush4932@users.noreply.github.com>
2021-12-09 12:39:47 +0530

    Update bug.ts

--- src/commands/General/bug.ts
+++ src/commands/General/bug.ts
@@ -1,8 +1,10 @@
-import { MessageType, Mimetype } from '@adiwajshing/baileys'
-import MessageHandler from '../../Handlers/MessageHandler'
-import BaseCommand from '../../lib/BaseCommand'
-import WAClient from '../../lib/WAClient'
-import { ISimplifiedMessage } from '../../typings'
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { IParsedArgs, ISimplifiedMessage } from "../../typings";
+//import { Character } from "mailist";
+import request from "../../lib/request";
+import { MessageType } from "@adiwajshing/baileys";
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
