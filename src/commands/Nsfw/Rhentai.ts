7aba84235a6d4205fd4ab1ee37f7cd407614e89c
SᴀʀᴜᴍᴀɴSᴀᴍᴀ <94067379+SarumanSama@users.noreply.github.com>
2021-12-09 09:28:23 +0530

    Update Rhentai.ts

--- src/commands/Nsfw/Rhentai.ts
+++ src/commands/Nsfw/Rhentai.ts
@@ -32,7 +32,7 @@ export default class Command extends BaseCommand {
                     MessageType.image,
                     undefined,
                     undefined,
-                    `🉐 *Name*: ${data.result.title}\n🔖 *Uploaded at*: ${data.result.uploaded_at}\n📖 *Code*: ${data.result.code_nuklir}\n👨🏻‍🎨 *Artist*: ${data.result.artist.name}\n🔰 *Follow artist:*${data.result.artist.count}\n${data.result.artist.url}\n  `,
+                    `🉐 *Name*: ${data.result.title}\n🔖 *Uploaded at*: ${data.result.uploaded_at}\n📖 *Code*: ${data.result.code_nuklir}\n👨🏻‍🎨 *Artist*: ${data.result.artist.name}\n🔰 *Follow artist:* ${data.result.artist.count}\n${data.result.artist.url}\n  `,
                     undefined
                 ).catch((e) => {
                     console.log(`This Error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
