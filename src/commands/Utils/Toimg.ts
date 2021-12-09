b6d39276b8283d552cb41f28ecd7c6cfb4e4ebcb
SᴀʀᴜᴍᴀɴSᴀᴍᴀ <94067379+SarumanSama@users.noreply.github.com>
2021-12-09 12:57:00 +0530

    Update Toimg.ts

--- src/commands/Utils/Toimg.ts
+++ src/commands/Utils/Toimg.ts
@@ -1,7 +1,9 @@
-import MessageHandler from '../Handlers/MessageHandler'
-import BaseCommand from '../lib/BaseCommand'
-import WAClient from '../lib/WAClient'
-import { IParsedArgs, ISimplifiedMessage } from '../typings'
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import { Sticker, Categories, StickerTypes } from 'wa-sticker-formatter'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
