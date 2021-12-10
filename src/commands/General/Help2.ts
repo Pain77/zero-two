935549f7f40f99d72c50fb30af00042403425690
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2021-12-10 16:38:11 +0000

    Update Help2.ts

--- src/commands/General/Help2.ts
+++ src/commands/General/Help2.ts
@@ -20,7 +20,7 @@ export default class Command extends BaseCommand {
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const zerotwo = 
-			"https://tenor.com/view/zero-two-gif-gif-19724460";
+			"https://c.tenor.com/hRnnpZ8eqykAAAPo/zero-two-gif.mp4";
 		return void this.client.sendMessage(
 			M.from,
 			{ url: zerotwo },
