0db9f6c74f648ede9c4e589a18328238b92af499
00300 <90952986+well300@users.noreply.github.com>
2021-12-13 23:05:01 +0530

    Create Help2.ts

--- src/commands/General/Help2.ts
+++ src/commands/General/Help2.ts
@@ -30,23 +30,23 @@ export default class Command extends BaseCommand {
 				mimetype: Mimetype.gif,
 				caption: `━━❰•𝙁𝙪𝙣•❱━━
 				
-🎄 ${this.client.config.prefix}ᴀᴅᴠɪᴄᴇ 
-🎄 ${this.client.config.prefix}ꜰᴀᴄᴛ 
-🎄 ${this.client.config.prefix}ᴊᴀɪʟ 
-🎄 ${this.client.config.prefix}ᴊᴏᴋᴇ 
-🎄 ${this.client.config.prefix}Qᴜᴏᴛᴇ 
-🎄 ${this.client.config.prefix}ʀᴇᴀᴄᴛ 
-🎄 ${this.client.config.prefix}ʀɪᴘ 
-🎄 ${this.client.config.prefix}ꜱʜɪᴘ
-🎄 ${this.client.config.prefix}ᴍᴇᴍᴇ 
-🎄 ${this.client.config.prefix}ᴛʀɪɢɢᴇʀ 
-🎄 ${this.client.config.prefix}ᴡᴀɴᴛᴇᴅ
-🎄 ${this.client.config.prefix}ᴛʀᴀsʜ 
-🎄 ${this.client.config.prefix}ǫᴜᴇsᴛɪᴏɴ 
-🎄 ${this.client.config.prefix}ᴅᴀʀᴇ 
-🎄 ${this.client.config.prefix}ᴛʀᴜᴛʜ 
-🎄 ${this.client.config.prefix}ᴄʜᴀᴛ
-🎄 ${this.client.config.prefix}ᴄʜᴇꜱꜱ
+❄ ${this.client.config.prefix}ᴀᴅᴠɪᴄᴇ 
+❄ ${this.client.config.prefix}ꜰᴀᴄᴛ 
+❄ ${this.client.config.prefix}ᴊᴀɪʟ 
+❄ ${this.client.config.prefix}ᴊᴏᴋᴇ 
+❄ ${this.client.config.prefix}Qᴜᴏᴛᴇ 
+❄ ${this.client.config.prefix}ʀᴇᴀᴄᴛ 
+❄ ${this.client.config.prefix}ʀɪᴘ 
+❄ ${this.client.config.prefix}ꜱʜɪᴘ
+❄ ${this.client.config.prefix}ᴍᴇᴍᴇ 
+❄ ${this.client.config.prefix}ᴛʀɪɢɢᴇʀ 
+❄ ${this.client.config.prefix}ᴡᴀɴᴛᴇᴅ
+❄ ${this.client.config.prefix}ᴛʀᴀsʜ 
+❄ ${this.client.config.prefix}ǫᴜᴇsᴛɪᴏɴ 
+❄ ${this.client.config.prefix}ᴅᴀʀᴇ 
+❄ ${this.client.config.prefix}ᴛʀᴜᴛʜ 
+❄ ${this.client.config.prefix}ᴄʜᴀᴛ
+❄ ${this.client.config.prefix}ᴄʜᴇꜱꜱ
 ───────────
 🎗 ɴᴏᴛᴇ: ᴜꜱᴇ ${this.client.config.prefix}ʜᴇʟᴘ <ᴄᴏᴍᴍᴀɴᴅ_ɴᴀᴍᴇ> ᴛᴏ ᴠɪᴇᴡ ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅ ɪɴꜰᴏ.
 ───────────    
