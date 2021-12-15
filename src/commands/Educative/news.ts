362b83ef38879e617926856f2a58f0cd39c85032
Pratyush <86536877+pratyush4932@users.noreply.github.com>
2021-12-15 11:57:14 +0530

    Update news.ts

--- src/commands/Educative/news.ts
+++ src/commands/Educative/news.ts
@@ -4,7 +4,7 @@ import MessageHandler from "../../Handlers/MessageHandler";
 import BaseCommand from "../../lib/BaseCommand";
 import WAClient from "../../lib/WAClient";
 import { IParsedArgs, ISimplifiedMessage } from "../../typings";
-
+import axios from 'axios'
 
 export default class Command extends BaseCommand {
 	constructor(client: WAClient, handler: MessageHandler) {
