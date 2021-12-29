cf1677641124624a7b0e586cebc8426d2e80dd42
00300 <90952986+well300@users.noreply.github.com>
2021-12-29 12:38:00 +0530

    Update 2Help.ts

--- src/commands/General/2Help.ts
+++ src/commands/General/2Help.ts
@@ -44,25 +44,24 @@ export default class Command extends BaseCommand {
                     .map((command) => command.config?.command)
                      .join(' \n')}\`\`\`\n\n`
             return void this.client.sendMessage(M.from, { url: zerotwo }, MessageType.image, {quoted:M.WAMessage,
-
-
-            caption: `${text} 📝 *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*` }
-            )
-        }
+            caption: `${text} 📝 *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*` }) }
+         
         const key = parsedArgs.joined.toLowerCase()
         const command = this.handler.commands.get(key) || this.handler.aliases.get(key)
         if (!command) return void M.reply(`No Command of Alias Found | "${key}"`)
         const state = await this.client.DB.disabledcommands.findOne({ command: command.config.command })
         M.reply(
-            `🚀 *Command:* ${this.client.util.capitalize(command.config?.command)}\n📉 *Status:* ${
-                state ? 'Disabled' : 'Available'
-            }\n⛩ *Category:* ${this.client.util.capitalize(command.config?.category || '')}${
-                command.config.aliases
-                    ? `\n♦️ *Aliases:* ${command.config.aliases.map(this.client.util.capitalize).join(', ')}`
-                    : ''
-            }\n🎐 *Group Only:* ${this.client.util.capitalize(
-                JSON.stringify(!command.config.dm ?? true)
-            )}\n💎 *Usage:* ${command.config?.usage || ''}\n\n📒 *Description:* ${command.config?.description || ''}`
+            `🚀 *Command:* ${this.client.util.capitalize(command.config?.command)}\n
+            
+             📉 *Status:* ${state ? 'Disabled' : 'Available'}\n
+             
+             ⛩ *Category:* ${this.client.util.capitalize(command.config?.category || '')}${command.config.aliases?`\n     
+             
+              ♦️ *Aliases:* ${command.config.aliases.map(this.client.util.capitalize).join(', ')}` : ''}\n
+              
+              🎐 *Group Only:* ${this.client.util.capitalize(JSON.stringify(!command.config.dm ?? true))}\n
+              
+             💎 *Usage:* ${command.config?.usage || ''}\n\n📒 *Description:* ${command.config?.description || ''}`
         )
     }
 
